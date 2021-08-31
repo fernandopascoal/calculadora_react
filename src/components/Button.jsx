b8bdecd69e190a4ad08244e05962cb457df16caf
Fernando Pascoal <fernandodevpascoal@gmail.com>
2021-08-31 17:11:26 -0300

    Projeto finalizado

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './Button.css'
 
 export default (props) => 
+
 <button 
 
-    onClick={(e) => props.click && (props.label)}
+    onClick={(e) => props.click && props.click(props.label)}
     className={`
         button
         ${props.operation ? 'operation' : ''}
-        ${props.operation ? 'double' : ''}
-        ${props.operation ? 'triple' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
     `}>
     {props.label}
 </button>
